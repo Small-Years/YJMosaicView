1c67e0b95bf810042546115bb9d62ffb4944fd22
yangjian <18263800587@163.com>
2019-01-28 17:15:17 +0800

    更改项目介绍
    完善项目

--- README.md
+++ README.md
@@ -3,13 +3,108 @@
 原理：原图按比例缩小到屏幕尺寸显示，手指移动将路径点重新乘上缩小的比例，然后实时显示的点对应原图上的点生成的图，并不是通过layer渲染的失真缩小图，所以经过此方法处理过的图片，不会存在图片编辑后尺寸变小、失真、分辨率降低等问题。
 
 效果图：
+![效果图](http://lc-nxp0vq41.cn-n1.lcfile.com/45ef8657369dc85e9a86.gif "效果图")
+
+### 实现功能：
+
+1. 马赛克画笔功能，可自定义马赛克图案 ，马赛克大小
+2. 编辑器可放大，放大后双指移动视图，单指画马赛克
+3. 上一步，下一步
+4. 实现了对原图做处理功能，并不是失真保存图案
+5. 优化了处理时 CPU 占用太高问题 
 
 
-实现功能：
-   1、马赛克画笔功能，可自定义马赛克图案，马赛克大小
-   2、编辑器可放大，放大后双指移动视图，单指画马赛克
-   3、上一步，下一步
-   4、实现了对原图做处理功能，并不是失真保存图案
-   5、优化了处理时 CPU 占用太高问题
 
+使用方式：
+
+1、将YJMosaiView文件夹拖入项目
+
+2、创建马赛克画板：
+```
+MosaiView *mosaicView = [[MosaiView alloc] initWithFrame:showRect];
+mosaicView.centerX = mainScrollView.width * 0.5;
+mosaicView.deleagate = self;
+mosaicView.originalImage = image;//原图
+mosaicView.mosaicImage = image;//马赛克图
+[self.view addSubview:mosaicView];
+```
+注意：mosaicImage 这个图是根据原图生成的马赛克图片，如果需要其他种类的马赛克图，只需要将新样式的马赛克图赋值给这个属性即可。
+
+
+3、双指放大操作，将MosaiView添加到一个UIScrollView上即可
+
+```
+/**
+创建马赛克画板
+
+@param image 原图
+@param frame 画板frame
+*/
+- (void)createMosaicsView:(UIImage *)image withFrame:(CGRect)frame {
+if (mainScrollView != nil) {
+[mainScrollView removeFromSuperview];
+mainScrollView = nil;
+}
+mainScrollView = [[UIScrollView alloc] initWithFrame:frame];
+
+mainScrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+mainScrollView.showsHorizontalScrollIndicator = NO;
+mainScrollView.showsVerticalScrollIndicator = NO;
+mainScrollView.delegate = self;
+mainScrollView.clipsToBounds = NO;
+mainScrollView.y = frame.origin.y;
+[self.view addSubview:mainScrollView];
+
+CGFloat img_Width = (image.size.width * mainScrollView.height)/image.size.height;
+CGRect showRect = CGRectMake(0, 0, img_Width, mainScrollView.height);
+if (image.size.width > image.size.height) {
+CGFloat img_Height = (image.size.height * mainScrollView.width)/image.size.width;
+showRect = CGRectMake(0, 0, mainScrollView.width,img_Height);
+showRect.origin.y = (mainScrollView.height - img_Height)*0.5;
+}
+
+
+self.mosaicView = [[MosaiView alloc] initWithFrame:showRect];
+self.mosaicView.centerX = mainScrollView.width * 0.5;
+self.mosaicView.deleagate = self;
+self.mosaicView.originalImage = image;//原图
+self.mosaicView.mosaicImage = image;//马赛克图
+[mainScrollView addSubview:self.mosaicView];
+
+mainScrollView.maximumZoomScale = 2.0;
+mainScrollView.minimumZoomScale = 1;
+mainScrollView.panGestureRecognizer.minimumNumberOfTouches = 2;
+mainScrollView.panGestureRecognizer.delaysTouchesBegan = NO;
+mainScrollView.pinchGestureRecognizer.delaysTouchesBegan = NO;   
+}
+```
+代理方法
+```
+
+#pragma mark -UIScrollViewDelegate
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+return self.mosaicView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+CGFloat Ws = mainScrollView.frame.size.width - mainScrollView.contentInset.left - mainScrollView.contentInset.right;
+CGFloat Hs = mainScrollView.frame.size.height - mainScrollView.contentInset.top - mainScrollView.contentInset.bottom;
+CGFloat W = mainScrollView.frame.size.width;
+CGFloat H = mainScrollView.frame.size.height;
+
+CGRect rct = self.mosaicView.frame;
+rct.origin.x = MAX((Ws-W)/2, (SCREEN_WIDTH - self.mosaicView.width)*0.5);
+rct.origin.y = MAX((Hs-H)/2, (mainScrollView.height - self.mosaicView.height)*0.5);
+self.mosaicView.frame = rct;
+}
+
+```
+
+详细介绍简书地址：
+
+https://www.jianshu.com/p/50c8eeb5badd
+
+更详细使用规则清查看demo，若此经验对你有所帮助，劳烦各位给个Star   (＾－＾)V  
 
--- YJMosaiDemo/DrawViewController.m
+++ YJMosaiDemo/DrawViewController.m
@@ -99,7 +99,14 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
-//创建马赛克画板
+
+
+/**
+ 创建马赛克画板
+
+ @param image 原图
+ @param frame 画板frame
+ */
 - (void)createMosaicsView:(UIImage *)image withFrame:(CGRect)frame {
     if (mainScrollView != nil) {
         [mainScrollView removeFromSuperview];
@@ -123,13 +130,12 @@
         showRect.origin.y = (mainScrollView.height - img_Height)*0.5;
     }
     
-    UIImage *showImage = [self mosaicImage:image mosaicLevel:20];
     
     self.mosaicView = [[MosaiView alloc] initWithFrame:showRect];
     self.mosaicView.centerX = mainScrollView.width * 0.5;
     self.mosaicView.deleagate = self;
     self.mosaicView.originalImage = image;//原图
-    self.mosaicView.mosaicImage = showImage;//马赛克图
+    self.mosaicView.mosaicImage = image;//马赛克图
     [mainScrollView addSubview:self.mosaicView];
     
     mainScrollView.maximumZoomScale = 2.0;
@@ -138,78 +144,6 @@
     mainScrollView.panGestureRecognizer.delaysTouchesBegan = NO;
     mainScrollView.pinchGestureRecognizer.delaysTouchesBegan = NO;
     
-}
-
-//生成原图马赛克图片
--(UIImage *)mosaicImage:(UIImage *)sourceImage mosaicLevel:(NSUInteger)level{
-    
-    //1、这一部分是为了把原始图片转成位图，位图再转成可操作的数据
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();//颜色通道
-    CGImageRef imageRef = sourceImage.CGImage;//位图
-    CGFloat width = CGImageGetWidth(imageRef);//位图宽
-    CGFloat height = CGImageGetHeight(imageRef);//位图高
-    CGContextRef context = CGBitmapContextCreate(nil, width, height, 8, width * 4, colorSpace, kCGImageAlphaPremultipliedLast);//生成上下午
-    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, width, height), imageRef);//绘制图片到上下文中
-    unsigned char *bitmapData = CGBitmapContextGetData(context);//获取位图的数据
-    
-    //2、这一部分是往右往下填充色值
-    NSUInteger index,preIndex;
-    unsigned char pixel[4] = {0};
-    for (int i = 0; i < height; i++) {//表示高，也可以说是行
-        for (int j = 0; j < width; j++) {//表示宽，也可以说是列
-            index = i * width + j;
-            if (i % level == 0) {
-                if (j % level == 0) {
-                    //把当前的色值数据保存一份，开始为i=0，j=0，所以一开始会保留一份
-                    memcpy(pixel, bitmapData + index * 4, 4);
-                }else{
-                    //把上一次保留的色值数据填充到当前的内存区域，这样就起到把前面数据往后挪的作用，也是往右填充
-                    memcpy(bitmapData +index * 4, pixel, 4);
-                }
-            }else{
-                //这里是把上一行的往下填充
-                preIndex = (i - 1) * width + j;
-                memcpy(bitmapData + index * 4, bitmapData + preIndex * 4, 4);
-            }
-        }
-    }
-    
-    //把数据转回位图，再从位图转回UIImage
-    NSUInteger dataLength = width * height * 4;
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, bitmapData, dataLength, NULL);
-    
-    CGImageRef mosaicImageRef = CGImageCreate(width, height,
-                                              8,
-                                              32,
-                                              width*4 ,
-                                              colorSpace,
-                                              kCGBitmapByteOrderDefault,
-                                              provider,
-                                              NULL, NO,
-                                              kCGRenderingIntentDefault);
-    CGContextRef outputContext = CGBitmapContextCreate(nil,
-                                                       width,
-                                                       height,
-                                                       8,
-                                                       width*4,
-                                                       colorSpace,
-                                                       kCGImageAlphaPremultipliedLast);
-    CGContextDrawImage(outputContext, CGRectMake(0.0f, 0.0f, width, height), mosaicImageRef);
-    CGImageRef resultImageRef = CGBitmapContextCreateImage(outputContext);
-    UIImage *resultImage = nil;
-    if([UIImage respondsToSelector:@selector(imageWithCGImage:scale:orientation:)]) {
-        float scale = [[UIScreen mainScreen] scale];
-        resultImage = [UIImage imageWithCGImage:resultImageRef scale:scale orientation:UIImageOrientationUp];
-    } else {
-        resultImage = [UIImage imageWithCGImage:resultImageRef];
-    }
-    CFRelease(resultImageRef);
-    CFRelease(mosaicImageRef);
-    CFRelease(colorSpace);
-    CFRelease(provider);
-    CFRelease(context);
-    CFRelease(outputContext);
-    return resultImage;
 }
 
 
